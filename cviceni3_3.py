09ce9ff02ccac93ee3aa77f3cfdfbd7d2bc656b7
cernej <cernej@email.cz>
2025-11-22 08:35:32 +0000

    cviceni3

--- cviceni3_3.py
+++ cviceni3_3.py
@@ -2,6 +2,12 @@ def jaccardova_vzdalenost_mnozin(mnozina1, mnozina2):
     """
     Jaccardova vzdalenost říká, jak jsou dvě množiny rozdílné, 0 znamená, že jsou stejné, 1 znamená, že jsou zcela rozdílné
     """
+    mnozina1 = set(mnozina1)
+    mnozina2 = set(mnozina2)
+    intersection = mnozina1.intersection(mnozina2)
+    union = mnozina1.union(mnozina2)
+    index = len(intersection) / len(union)
+    return 1 - index
 
 
 if __name__ == "__main__":
