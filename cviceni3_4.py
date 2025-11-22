f534e4dc11b8f027efc8b9cb5f2c164dee6fd02a
cernej <cernej@email.cz>
2025-11-22 09:04:12 +0000

    cviceni3

--- cviceni3_4.py
+++ cviceni3_4.py
@@ -1,5 +1,5 @@
-from cviceni3_2 import jaccardova_vzdalenost_mnozin
-from cviceni3_3 import levensteinova_vzdalenost
+from cviceni3_2 import levensteinova_vzdalenost
+from cviceni3_3 import jaccardova_vzdalenost_mnozin
 
 
 def deduplikace_dotazu(dotazy):
@@ -7,8 +7,19 @@ def deduplikace_dotazu(dotazy):
     tato funkce spocita jaccardovu vzdalenost a levensteinovu vzadelnost a vyradi z seznamu dotazy, polozky, ktere budou mit
     jaccardovu vzdalenost mensi nez 0.5 a levensteinovu vzdalenost <= 1
     """
-
-
+    i = 0
+    while i < len(dotazy):
+        j = i + 1
+        while j < len(dotazy):
+            lev = levensteinova_vzdalenost(dotazy[i]["dotaz"], dotazy[j]["dotaz"])
+            jac = jaccardova_vzdalenost_mnozin(dotazy[i]["serp"], dotazy[j]["serp"])
+            if lev <= 1 or jac < 0.5:
+                dotazy.pop(j)
+            else:
+                j += 1
+        i += 1
+    return dotazy
+            
 
 if __name__ == "__main__":
 
@@ -17,11 +28,11 @@ if __name__ == "__main__":
         "serp": ["https://www.seznam.cz", "https://www.jcu.cz", "https://www.czu.cz", "https://www.cvut.cz", "https://www.uk.cz", "https://www.google.com"]
     }
     dotaz2 = {
-        "dotaz": "seznamka",
+        "dotaz": "sesnam",
         "serp": ["https://www.seznam.cz", "https://www.google.com", "https://www.novinky.cz", "https://www.idnes.cz", "https://www.zpravy.cz", "https://www.tn.cz"]
     }
     dotaz3 = {
-        "dotaz": "sesnam",
+        "dotaz": "seznamka",
         "serp": ["https://www.jcu.cz", "https://www.czu.cz", "https://www.cvut.cz", "https://www.uk.cz"]
     }
     dotaz4 = {
